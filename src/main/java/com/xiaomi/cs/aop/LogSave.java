910bbe7ec59286e18f01560f6a200f456e7c91bd
lcyoko <m15879038981@163.com>
2020-11-05 14:23:53 +0800

    '201105-cy-login'

---
+++ src/main/java/com/xiaomi/cs/aop/LogSave.java
@@ -0,0 +1,9 @@
+package com.xiaomi.cs.aop;
+
+/**
+ * @author l
+ * @create 2020-11-05-14:23
+ */
+public class LogSave {
+
+}
--- src/main/java/com/xiaomi/cs/service/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.xiaomi.cs.service;
-
-/**
- * @author l
- * @create 2020-11-05-11:08
- */
-public class UserServiceImpl {
-}
